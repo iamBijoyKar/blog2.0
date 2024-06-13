c38a5ad106996e9bd7a10e47397b280ff53d831a
Bijoy Kar <bijoykar54321@gmail.com>
2024-06-13 23:08:00 +0530

    [chore]: Update npm dependencies and Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), svelte(), sitemap()],
   site: "https://iamBijoyKar.github.io",
   // base: "/blog2.0",
-  output: "server",
+  output: "static",
   adapter: vercel(),
 });
